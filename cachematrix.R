9d4b97fcbeb449d246f8c4519bd1f5f0024660c3
Thomaz Lago Santana <thomazwrx@gmail.com>
2015-01-25 17:19:38 -0500

    Cache Matrix Inverse
    These functions create a special "matrix" object that can mutate a
    matrix and cache its inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a special "matrix" object that can mutate a matrix 
+## and cache its inverse.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrix_inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    matrix_inverse <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(inverse) matrix_inverse <<- inverse
+  get_inverse <- function() matrix_inverse
+  list(set=set, get=get, set_inverse=set_inverse,
+       get_inverse=get_inverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes & returns the inverse of the special "matrix" 
+## object if the inverse has not yet been computed; else it returnes the
+## cached inverse.
+## If the matrix is not inversible, NULL is returned along with a message. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matrix_inverse <- x$get_inverse()
+  if(!is.null(matrix_inverse)) {
+    message('Getting cached inverse...')
+  }
+  matrix <- x$get()
+  inverse <- tryCatch({
+    solve(matrix)
+  },
+  error = function(e) {
+    message(e$message)
+    NULL
+  }
+  )
+  x$set_inverse(inverse)
+  inverse
 }
